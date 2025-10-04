f9074d98498902a4b0cb5bd5cc435c61525145d1
WadersonSantos <blackdoacordeon@gmail.com>
2025-10-04 02:11:39 -0300

    Implementing user roles

--- database.py
+++ database.py
@@ -1,2 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
+
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql:///root:admin123@127.0.0.1:3306/flask-crud'
+# app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://admin:admin123@db:3306/flask-crud"
+
+# .\venv\Scripts\activate --> THIS IS TO GET INTO MY VENV.
